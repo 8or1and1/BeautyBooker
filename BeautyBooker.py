37ba6306f30ab4217d16496f3bdcbe73562c6c8f
8or1and1 <8or1and1@gmail.com>
2023-04-12 04:37:01 +0300

    Clean DB

--- BeautyBooker.py
+++ BeautyBooker.py
@@ -1,4 +1,9 @@
 from flask import Flask
-from app import app
+from app import app, db
+from app.models import User, Master
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Master}
 
 Flask.run(app)
